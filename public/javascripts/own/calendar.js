0f9eefae677bcf4519af52018504a16577d50a33
Alevardi <Amet.Alvirde@gmail.com>
2013-07-04 11:07:35 -0500

    prepairing merge

--- public/javascripts/own/calendar.js
+++ public/javascripts/own/calendar.js
@@ -5,11 +5,22 @@ YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
       showPrevMonth: true,
       showNextMonth: true,
       date: new Date()}).render();    
-    var dtdate = Y.DataType.Date;    
+    var dtdate = Y.DataType.Date; 
+
     
+    // calendar.cfg.setProperty("MONTHS_SHORT",   ["Ene", "Feb", "Mar", "Abr", "Mayo", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]);
+    // calendar.cfg.setProperty("MONTHS_LONG",    ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]);
+    // calendar.cfg.setProperty("WEEKDAYS_1CHAR", ["D", "L", "Ma", "Mi", "J", "V", "S"]);
+    // calendar.cfg.setProperty("WEEKDAYS_SHORT", ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"]);
+    // calendar.cfg.setProperty("WEEKDAYS_MEDIUM",["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"]);
+    // calendar.cfg.setProperty("WEEKDAYS_LONG",  ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]);
+    
+
+        
     calendar.on("selectionChange", function (ev) {     
       var newDate = ev.newSelection[0];
       Y.one("#selecteddate").setHTML(dtdate.format(newDate));
     });
 
 });
+
